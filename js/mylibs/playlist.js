3d117a758430a17fc371f5197779330c8557e9ef
Pierre Larochelle <pierrebombay@gmail.com>
2010-12-21 22:57:39 -0800

    cleaned up the interface a bit, added playlist change event and playlist/current song rendering

--- js/mylibs/playlist.js
+++ js/mylibs/playlist.js
@@ -1,16 +1,26 @@
 Playlist = function(the_songs) {
 	var songs = the_songs;
 	var index = 0;
+	var changed_func = null;
 	
 	return {
+		changed: function(func) { //function(songs, index)
+			changed_func = func;
+		},
 		songs: function() {
 			return songs;
+		},
+		replace_with: function(new_songs) {
+			index = 0;
+			songs = new_songs;
+			changed_func(songs, index);
 		},
 		current: function() {
 			return songs[index];
 		},
 		next: function() {
 			index += 1;
+			changed_func(songs, index);
 			return this.current();
 		}
 	}
